9d85939f03734f411c608d5e707045baf33e3bf8
Reynaldy Hardiyanto <reynaldy.hardiyanto@gmail.com>
2018-05-08 00:42:56 +0700

    first commit

---
+++ Tutorial Mat 3/src/main.java
@@ -0,0 +1,30 @@
+
+public class main {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AVLTree tree = new AVLTree();
+		 
+        /* Constructing tree given in the above figure */
+        tree.root = tree.insert(tree.root, 10);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 40);
+        tree.root = tree.insert(tree.root, 50);
+        tree.root = tree.insert(tree.root, 25);
+ 
+        /* The constructed AVL Tree would be
+             30
+            /  \
+          20   40
+         /  \     \
+        10  25    50
+        */
+        System.out.println("Preorder traversal" +
+                        " of constructed tree is : ");
+        tree.preOrder(tree.root);
+
+	}
+
+}
